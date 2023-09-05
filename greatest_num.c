0d6da34b3000766a1a447505c8f22f02b08de18d
HASHIMWANI <136843214+HASHIMWANI@users.noreply.github.com>
2023-09-05 11:42:47 +0530

    Add files via upload

---
+++ greatest_num.c
@@ -0,0 +1,11 @@
+/* Determine greatest of 3 numbers using conditional operators */
+# include <stdio.h>
+int main( )
+{
+int n1, n2, n3, great ;
+printf ( "\nEnter three numbers: " ) ;
+scanf ( "%d %d %d", &n1, &n2, &n3 ) ;
+great = n1 > n2 ? ( n1 > n3 ? n1 : n3 ) : ( n2 > n3 ? n2 : n3 ) ;
+printf ( "Greatest number is: %d\n", great ) ;
+return 0 ;
+}
